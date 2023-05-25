ff328ed93e82f1dcfbc3a5ce75f85a24deb33720
2021122760232 <2021122760232@col-8104-li07.cefetes.br>
2023-05-25 14:59:34 -0300

    Ajustes

---
+++ x-fome/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into categoria(nome) values ('Sobremesa'), ('Bebidas'), ('Hamburguers');
+
+insert into status(nome) values ('Pendente'), ('Aceito'), ('Retirada na loja'), ('Pedido rejeitado'), ('Entregue'), ('A caminho'), ('Em processamento');
+
+insert into usuario(cep, cidade, cpf, email, endereco, isCliente, nomeUsuario, senha)
+values ('27000-00', 'Colatina', '111.222.333-89', 'funcionario@gmail.com', 'Rua tal', 0, 'Fulano', '123');
